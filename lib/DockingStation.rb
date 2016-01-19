aedd8747d74064ea890972ad4f346744aa54eee8
Kait Beaudette <Kait Beaudette>
2016-01-19 12:18:37 -0600

    day-one challenges through 11

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,2 +1,15 @@
 class DockingStation
+	def release_bike
+		Bike.new
+	end
+
+	def dock(bike)
+		@docked_bike = bike
+	end
+
+	def bike
+		@docked_bike
+	end
+
+	attr_reader (:docked_bike)
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,7 +1,18 @@
 require 'DockingStation'
 
 describe DockingStation do
-  #it 'returns an object when passed docking_station' do
-  #  expect(docking_station).to eq DockingStation.new
-  #end
+#		it 'Responds to release_bike' do
+#		expect(DockingStation).to respond_to(release_bike)
+	it { should respond_to(:release_bike)}
+
+	station = DockingStation.new
+	bike = station.release_bike
+
+	it { expect(bike.working?).to eq true}
+
+	it { expect(station).to respond_to(:dock).with(1).argument }
+
+	it { expect(station.dock(bike)).to eq(bike) }
+
+	it { expect(station.bike).to eq(bike) }
 end
