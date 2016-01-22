4410df5960578c0cd0dc1ace45de4afd4ca0b14e
Finble <elainefinn@hotmail.com>
2016-01-22 12:03:10 +0000

    Ch11 completed

--- boris-bikes/README.md
+++
@@ -1,9 +0,0 @@
-Boris Bikes - Version 1 (19/01/2016 @ 1:57PM GMT) - Designed to run on Ruby
-
-Written by Mat Drake and Alain Lemaire
-
-This program will run the docking stations, including simulating the bikes and running the infrastructure, for the new Boris Bikes in London.
-
-Includes rspec testing files.
-
-For the Horde!
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,34 @@
 require 'docking_station'
 
 RSpec.describe DockingStation do
-  it { should respond_to(:release_bike) }
+
+    it { expect respond_to(:release_bike) }
+
+    docking = DockingStation.new
+
+    it 'return an instance of Bike' do
+    expect(subject.release_bike).to be_a(Bike)
+	  end
+
+	  bike = Bike.new
+
+	  it 'expects bike to respond to working?' do
+		expect(bike.working?).to eq(true)
+	  end
+
+    it { expect respond_to(:@bike) }
+
+    it 'returns a docked bike' do
+    expect(subject.dock(bike)).to eq bike
+    end
+
+    it do print "#{(subject.dock(bike))}" end
+
+    it {is_expected.to respond_to(:dock).with(1).argument }
+
+    it 'returns an instance variable' do
+    expect(subject.instance_variable_get(:@bike)) == (subject.dock(Bike.new))
+    end
+
+    it do print "#{(docking.bike)}" end
 end
