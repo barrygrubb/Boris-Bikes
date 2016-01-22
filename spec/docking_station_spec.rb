38f934dcf1d388afcd94c19f28f1f084b20139f3
Finble <elainefinn@hotmail.com>
2016-01-22 17:06:58 +0000

    Ch12 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,16 @@
 require 'docking_station'
 
-RSpec.describe DockingStation do
+describe DockingStation do
 
     it { expect respond_to(:release_bike) }
 
     docking = DockingStation.new
 
-    it 'return an instance of Bike' do
-    expect(subject.release_bike).to be_a(Bike)
-	  end
+   #  it 'return an instance of Bike' do
+   #  expect(subject.release_bike).to be_a(Bike)
+	  # end
+
+
 
 	  bike = Bike.new
 
@@ -22,7 +24,7 @@ RSpec.describe DockingStation do
     expect(subject.dock(bike)).to eq bike
     end
 
-    it do print "#{(subject.dock(bike))}" end
+    # it do print "#{(subject.dock(bike))}" end
 
     it {is_expected.to respond_to(:dock).with(1).argument }
 
@@ -30,5 +32,22 @@ RSpec.describe DockingStation do
     expect(subject.instance_variable_get(:@bike)) == (subject.dock(Bike.new))
     end
 
-    it do print "#{(docking.bike)}" end
+    # it do print "#{(docking.bike)}" end
+
+    describe '#release_bike' do
+      it 'returns error' do
+      expect{subject.release_bike}.to raise_error('no bikes available')
+    end
+
+  # def release_bike
+  #   fail 'no bikes available' unless @bike
+  #   @bike
+  # end
+
+    # it 'if no bikes, do not return a bike' do
+    #   expect(subject.release_bike).to not_be_a(Bike)
+    # end
+    end
 end
+
+
