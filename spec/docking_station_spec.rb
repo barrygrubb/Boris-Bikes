b74e545d0ab47a801e113c4c4534f4ca0cfa1926
Finble <elainefinn@hotmail.com>
2016-01-23 12:13:06 +0000

    Ch 16 completed - defined Default_Capacity Constant and refactored code

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -44,7 +44,7 @@ describe DockingStation do
 
     describe '#dock_bikes' do
        it 'returns error' do
-         20.times {subject.dock(Bike.new)}
+         DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
        expect{subject.dock(bike)}.to raise_error('station is full')
     end
 
