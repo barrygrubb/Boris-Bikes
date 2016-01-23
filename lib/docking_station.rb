ad0edfdedaa2bf846e618e26322f5c719a4cbfa2
Finble <elainefinn@hotmail.com>
2016-01-23 12:00:24 +0000

    Ch 15 completed, private methods empty? full? added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,13 +7,39 @@ class DockingStation
   end
 
   def release_bike
-    fail 'no bikes available' unless @bikes.length >= 1
+    fail 'no bikes available' if empty?
     @bikes.pop 
   end
 
   def dock(bike)  
-    fail 'station is full' unless @bikes.length < 20
+    fail 'station is full' if full?
     @bikes << bike
   end
 
+private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.count == 0
+  end
 end
+
+# below code refactored to above:
+  # def full?
+  #   if @bikes.length < 20
+  #     false
+  #   else
+  #     true
+  #   end
+  # end
+
+  # def empty?
+  #   if @bikes.length >= 1
+  #     false
+  #   else
+  #     true
+  #   end
+  # end
