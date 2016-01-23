f69f9f62857b95491902fb6a6e78b50dbab8e1e3
Matthew Drake <matdrake@live.co.uk>
2016-01-23 22:23:43 +0000

    Added tests for van transportation

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -6,8 +6,20 @@ describe Van do
     expect(subject).to be_a(Van)
   end
 
-  it 'responds to collect' do
-    expect(subject).to respond_to(:collect).with(1).argument
+  it 'responds to collect_from_dock' do
+    expect(subject).to respond_to(:collect_from_dock).with(1).argument
+  end
+
+  it 'responds to collect_from_garage' do
+    expect(subject).to respond_to(:collect_from_garage).with(1).argument
+  end
+
+  it 'responds to deliver_to_dock' do
+    expect(subject).to respond_to(:deliver_to_dock).with(1).argument
+  end
+
+  it 'responds to deliver_to_garage' do
+    expect(subject).to respond_to(:deliver_to_garage).with(1).argument
   end
 
   it 'has an array for bikes' do
